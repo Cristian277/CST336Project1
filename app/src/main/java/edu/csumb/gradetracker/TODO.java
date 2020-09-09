a9fcf8fb84c976554a11a3ea384332ced1119bc4
Jonathan Welham <jwelham@csumb.edu>
2020-09-08 17:40:32 -0700

    Created TODO Class for documentation

---
+++ app/src/main/java/edu/csumb/gradetracker/TODO.java
@@ -0,0 +1,10 @@
+//package edu.csumb.gradetracker;
+//
+//class TODO {
+//}
+
+
+//TODO: Assignment still needs to include a few variables not yet coded
+//TODO: Create grade class in Model
+//TODO: Work on course/assignment/grade
+//TODO:
